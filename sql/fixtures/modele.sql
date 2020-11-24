bebfa802ac8bca6d912f2ae59cfebc4a726f05ee
lareko <cdh@nst.fr>
2020-11-24 16:10:42 +0100

    modele suite alex

--- sql/fixtures/modele.sql
+++ sql/fixtures/modele.sql
@@ -1,2 +1,9 @@
 INSERT INTO `modele` (`modele_id`, `nom`, `date`, `prix`) VALUES
 (1, 'Vaporwaffle sacai Black White', '24/11/2020', '300 €'),
+
+
+/* ALEXANDER MCQUEEN */
+(53, 'Oversized Clear Sole Black', '01/09/2020', '477 €'),
+(52, 'Oversized Red', '01/09/2020', '477 €'),
+(53, 'Oversized Clear Sole Black', '07/01/2020', '477 €'),
+(54, 'Oversized Iridescent', '01/09/2020', '495 €');
