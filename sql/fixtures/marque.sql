39956ceb48d073740bf8e7dacd5767f77e11fc2e
lareko <cdh@nst.fr>
2020-11-24 15:19:20 +0100

    ajout fichier sql

---
+++ sql/fixtures/marque.sql
@@ -0,0 +1,9 @@
+INSERT INTO `marque` (`marque_id`, `nom`, `histoire`) VALUES
+(1, 'Nike', 'Le nom Nike est inspiré de la déesse grecque de la victoire Niké (Nικη), déesse ailée capable de se déplacer à grande vitesse, dont la représentation la plus connue, une statue exposée au Louvre, est la Victoire de Samothrace.'),
+(2, 'Adidas', "Elle est mondialement connue sous l'appellation la marque aux trois bandes, des trois bandes parallèles qui constituent son logo.");
+(3, 'Jordan', '')
+(4, 'Balienciaga', '')
+(5, 'Converse', '')
+(6, 'Asics', '')
+(7, 'Alexander Mcqueen', '')
+
---
+++ sql/fixtures/modele.sql
@@ -0,0 +1,2 @@
+INSERT INTO `modele` (`modele_id`, `nom`, `date`, `prix`) VALUES
+(1, 'Vaporwaffle sacai Black White', '24/11/2020', '300 €'),
