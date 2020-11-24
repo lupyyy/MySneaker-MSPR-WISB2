432a4f0df2fa95abe5fa2f46858b94f7b06ef694
lareko <cdh@nst.fr>
2020-11-24 15:43:41 +0100

    marque sql terminé

--- sql/fixtures/marque.sql
+++ sql/fixtures/marque.sql
@@ -1,9 +1,9 @@
 INSERT INTO `marque` (`marque_id`, `nom`, `histoire`) VALUES
 (1, 'Nike', 'Le nom Nike est inspiré de la déesse grecque de la victoire Niké (Nικη), déesse ailée capable de se déplacer à grande vitesse, dont la représentation la plus connue, une statue exposée au Louvre, est la Victoire de Samothrace.'),
-(2, 'Adidas', "Elle est mondialement connue sous l'appellation la marque aux trois bandes, des trois bandes parallèles qui constituent son logo.");
-(3, 'Jordan', '')
-(4, 'Balienciaga', '')
-(5, 'Converse', '')
-(6, 'Asics', '')
-(7, 'Alexander Mcqueen', '')
+(2, 'Adidas', "Elle est mondialement connue sous l'appellation la marque aux trois bandes, des trois bandes parallèles qui constituent son logo."),
+(3, 'Jordan', 'Nike Air Jordan est une filiale de chaussures de basket-ball de la marque Nike, frappées du nom de Michael Jordan, ancien joueur des Bulls de Chicago en NBA.'),
+(4, 'Balienciaga', "Balenciaga est une entreprise française de mode et de luxe fondée en 1917 par l'Espagnol Cristóbal Balenciaga."),
+(5, 'Converse', "Converse fabrique des sneakers Chuck Taylor All Star et One Star depuis les débuts de la marque il y a plus d'un siècle, et désormais nous nous efforçons de concevoir des modèles qui font partie intégrante de la culture urbaine."),
+(6, 'Asics', "Asics est l'acronyme de l'expression latine de Juvénal « Anima Sana In Corpore Sano », en français « un esprit sain dans un corps sain »."),
+(7, 'Alexander Mcqueen', 'Alexander Mc Queen est un célèbre créateur de mode britannique et le nom de la marque qu’il a fondée en 1992.');
 
