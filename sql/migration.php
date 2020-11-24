7c0bebd0aeeb1b7c50cff73ec4e5c04cef5f9a9e
lupyyyy <vincent.richir@gmx.fr>
2020-11-24 14:59:57 +0100

    modification migration.php

--- sql/migration.php
+++ sql/migration.php
@@ -1,10 +1,21 @@
 <?php
-    require_once "../src/connexion.php";
-    $sql = file_get_contents('migrations/20201117_1704.sql');
+    require "../src/connect_database.php";
+    $date = "20201124_1420";
+    $versions = "migrations-versions.txt";
+    
+    if( strpos(file_get_contents($versions),$date) !== true) {
+        $sql = file_get_contents('migrations/' . $date . '.sql');
 
-    $res = $dbh->exec($sql);
-    if (!$res) {
+        $res = $dbh->exec($sql);
+        if (!$res) {
         echo "\nPDO::errorInfo():\n";
         print_r($dbh->errorInfo());
+        }
+        else {
+            echo "Migration effectuée avec succès !";
+        file_put_contents($versions, $date . "\n", FILE_APPEND);
+        }
     }
-?>
+    else {
+        echo "Migration déjà effectuée !";
+    }
