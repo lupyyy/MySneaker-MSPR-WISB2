d7c499f8542cc2eac8ab3f7c3342ddd84b38101f
lupyyyy <vincent.richir@gmx.fr>
2020-11-24 15:18:03 +0100

    modification php

--- sql/migration.php
+++ sql/migration.php
@@ -3,17 +3,17 @@
     $date = "20201124_1420";
     $versions = "migrations-versions.txt";
     
-    if( strpos(file_get_contents($versions),$date) !== true) {
+    if(!file_exists($versions) || strpos(file_get_contents($versions),$date) === false) {
         $sql = file_get_contents('migrations/' . $date . '.sql');
 
         $res = $dbh->exec($sql);
-        if (!$res) {
-        echo "\nPDO::errorInfo():\n";
-        print_r($dbh->errorInfo());
+        if ($res === false) {
+            echo "\nPDO::errorInfo():\n";
+            print_r($dbh->errorInfo());
         }
         else {
             echo "Migration effectuée avec succès !";
-        file_put_contents($versions, $date . "\n", FILE_APPEND);
+            file_put_contents($versions, $date . "\n", FILE_APPEND);
         }
     }
     else {
