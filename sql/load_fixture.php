c0cdcef9de19df762f8c870fdcb87b90ad274dba
lupyyyy <vincent.richir@gmx.fr>
2020-12-01 14:23:38 +0100

    ajout fixtures

--- sql/load_fixture.php
+++ sql/load_fixture.php
@@ -1,25 +1,29 @@
 <?php
-    require "../src/connect_database.php";
-    // TODO ici il faudra faire une boucle pour prendre tous les fichiers fixtures
-    $fichiers = ["modele.sql", "marque.sql"]; // etc.
-    $sql = file_get_contents('fixtures/fixtures.sql');
-    
-    //$res = $dbh->exec($sql);
-    try {
-        $dbh->beginTransaction();
-    
-        $res = $dbh->exec($sql);
-    
-        $dbh->commit();
-    } catch(Exception $e) {
-        $dbh->rollback();
-        throw $e;
-    }
-   
-    if ($res === false) {
-        echo "\nPDO::errorInfo():\n";
-        print_r($dbh->errorInfo());
-    }
-    else {
-        echo "Fixtures chargées avec succès !";
-    }
+require "../src/connect_database.php";
+$sql = '';
+$fichiers = ["modele.sql", "marque.sql", "utilisateur.sql", "appartient.sql", "souhaite.sql", "possede.sql"];
+
+foreach ($fichiers as $fichier) {
+    $sql .= file_get_contents('fixtures/' . $fichier);
+} echo $sql; 
+exit;
+
+
+//$res = $dbh->exec($sql);
+try {
+    $dbh->beginTransaction();
+
+    $res = $dbh->exec($sql);
+
+    $dbh->commit();
+} catch (Exception $e) {
+    $dbh->rollback();
+    throw $e;
+}
+
+if ($res === false) {
+    echo "\nPDO::errorInfo():\n";
+    print_r($dbh->errorInfo());
+} else {
+    echo "Fixtures chargées avec succès !";
+}
