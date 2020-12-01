fb9a1f3e902c8d0c4560bb12cdd06e33a30dcc46
lupyyyy <vincent.richir@gmx.fr>
2020-12-01 14:54:27 +0100

    ajut fixture

--- sql/fixtures/possede.sql
+++ sql/fixtures/possede.sql
@@ -1,2 +1,7 @@
 INSERT INTO `possede` (`user_id`, `modele_id`) VALUES
-(1,1);
+(1,23),
+(1,50),
+(2,6),
+(2,14),
+(2,2),
+(3,19);
--- sql/fixtures/souhaite.sql
+++ sql/fixtures/souhaite.sql
@@ -1,2 +1,8 @@
 INSERT INTO `souhaite` (`user_id`, `modele_id`) VALUES
-(1,1);
+(1,13),
+(1,53),
+(2,8),
+(2,45),
+(2,2),
+(3,33);
+
